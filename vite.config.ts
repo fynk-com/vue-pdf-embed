897f5a31ac788402effeb43a7918d46239740052
Constantin Wintoniak <wintoniak@fynk.com>
2026-02-03 18:56:13 +0100

    Fix build and css import

--- vite.config.ts
+++ vite.config.ts
@@ -20,24 +20,34 @@ export default defineConfig({
   plugins: [
     copy({
       hook: 'writeBundle',
-      targets: Object.entries({
-        textLayer: [
-          [3103, 3122],
-          [582, 709],
-        ],
-        annotationLayer: [
-          [3103, 3122],
-          [710, 1074],
-        ],
-      }).map(([key, ranges]) => ({
+      targets: (['textLayer', 'annotationLayer'] as const).map((key) => ({
         src: 'node_modules/pdfjs-dist/web/pdf_viewer.css',
         dest: 'dist/styles',
         rename: `${key}.css`,
         transform: (contents) => {
           const lines = contents.toString().split('\n')
-          const css = ranges.reduce((acc, [start, end]) => {
-            return acc + lines.slice(start, end).join('\n')
-          }, '')
+
+          const extractBetween = (startRe: RegExp, endRe: RegExp): string => {
+            const start = lines.findIndex((l) => startRe.test(l))
+            if (start < 0) {
+              throw new Error(
+                `Failed to find start marker (${startRe}) in pdf_viewer.css`
+              )
+            }
+
+            const endRel = lines
+              .slice(start + 1)
+              .findIndex((l) => endRe.test(l))
+            const end = endRel < 0 ? lines.length : start + 1 + endRel
+
+            return lines.slice(start, end).join('\n')
+          }
+
+          const css =
+            key === 'textLayer'
+              ? extractBetween(/^\.textLayer\{/, /^\.annotationLayer\{/)
+              : extractBetween(/^\.annotationLayer\{/, /^\.xfaLayer\{/)
+
           return new CleanCSS().minify(css).styles + '\n'
         },
       })),
